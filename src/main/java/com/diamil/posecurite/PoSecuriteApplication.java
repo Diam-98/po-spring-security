433f4de3dfd8a04cff140d476d30ad2feeb633a5
Diam Diallo <diamdiallo98@gmail.com>
2023-08-11 21:12:40 +0000

    first commit

---
+++ src/main/java/com/diamil/posecurite/PoSecuriteApplication.java
@@ -0,0 +1,13 @@
+package com.diamil.posecurite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoSecuriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoSecuriteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diamil/posecurite/PoSecuriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diamil.posecurite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoSecuriteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
